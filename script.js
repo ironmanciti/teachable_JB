4b9b981f8cb770845b48c2cbc1631aa476971e76
Young-Jea Oh <ironmanciti@gmail.com>
2020-12-09 19:23:13 +0900

    jb

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = 'https://teachablemachine.withgoogle.com/models/fN7eaHqzB/';
+let imageModelURL = 'https://teachablemachine.withgoogle.com/models/87Zpt5W4z/';
 
 // Video
 let video;
@@ -16,11 +16,11 @@ function preload() {
 }
 
 function setup() {
-  let cnv = createCanvas(320, 260);
+  let cnv = createCanvas(420, 360);
   cnv.parent("canvas");
   // Create the video
   video = createCapture(VIDEO);
-  video.size(320, 240);
+  video.size(420, 340);
   video.hide();
 
   input = createInput();
@@ -37,16 +37,25 @@ function draw() {
   // Draw the video
   image(flippedVideo, 0, 0);
 
-  if (label == "Saying Words") {
+  if (label == "words") {
     fill(255);
     textSize(20);
-    text(input.value(), width / 2, height / 2);
-  } else {
-    fill(255);
-    textSize(16);
-    text('', width / 2, height / 2);
-  }
-
+    text(input.value(), width / 2 - 30, height / 2);
+  } else if (label == "ok"){
+    fill(0, 255, 0);
+    textSize(50);
+    text("OK!", width / 2 - 10, 50);
+  } else if (label == "no"){
+    fill(255, 0, 0);
+    textSize(50);
+    text("NO...", width / 2 - 10, 50);
+  } else if (label == "raise"){
+    fill(0, 0, 255);
+    textSize(20);
+    textStyle(BOLD);
+    text("I have something to say", 200, 50);
+  } 
+  
   // Draw the label
   fill(255);
   textSize(16);
